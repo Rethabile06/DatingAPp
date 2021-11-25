8c38074b4063751a5446a3db310a1efd8dc478f8
Rethabile Tlali <tlalirethabile3@gmail.com>
2021-11-25 15:18:01 +0200

    End of setion 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,7 +35,7 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
-    
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +51,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
             app.UseAuthorization();
 
